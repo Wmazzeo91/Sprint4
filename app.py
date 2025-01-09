63a0be5322047ddb5a60dcd5b24342617a3fcbb9
Wmazzeo91 <wmazzeo91@gmail.com>
2025-01-08 23:48:39 -0600

    Fixing error 3

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-import streamlit as st
-import pandas as pd
-
-st.title('My first app')
-
-st.write("Hello, world!")
-
-st.write("Hello, again!")
-
-import streamlit as st
-
-st.header('Tossing a Coin')
-
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
-
-if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
-
-st.write('It is not a functional application yet. Under construction.')
-
-
 import pandas as pd
 import scipy.stats
 import streamlit as st
@@ -34,24 +12,22 @@ if 'df_experiment_results' not in st.session_state:
 
 st.header('Tossing a Coin')
 
-# Initialize with DataFrame
-chart_data = pd.DataFrame([0.5], columns=["Probability"])
-chart = st.line_chart(chart_data)
+chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
     outcome_no = 0
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-
-        # Ensure data is a DataFrame
-        new_data = pd.DataFrame([mean], columns=["Probability"])
-        chart.add_rows(new_data)
+        chart.add_rows([mean])
         time.sleep(0.05)
 
     return mean
